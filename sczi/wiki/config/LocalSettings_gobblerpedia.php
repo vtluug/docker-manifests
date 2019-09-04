451ec018c366567b0ac4dbfda415722b1846e38a
Paul Walko <pew@vtluug.org>
2019-09-03 20:27:05 -0400

    Disable google auth on luugwiki, fix gobblerpedia auth

--- sczi/wiki/config/LocalSettings_gobblerpedia.php
+++ sczi/wiki/config/LocalSettings_gobblerpedia.php
@@ -10,6 +10,15 @@ $wgServer = 'https://gobblerpedia.org';
 $wgLogo = "{$wgUploadPath}/gobblerpedia.png";
 $wgFavicon = "{$wgUploadPath}/favicon.png";
 
+# Google => Gobblerpedia only
+$wgPluggableAuth_ButtonLabelMessage = 'VTLUUG/Google SSO';
+$wgOpenIDConnect_Config['https://accounts.google.com'] = [
+    'clientID'     => getenv('ODIC_GOOGLE_ID'),
+    'clientsecret' => getenv('ODIC_GOOGLE_SECRET'),
+    'name'         => 'Google SSO',
+    'scope'        => ['openid', 'profile', 'email']
+];
+
 ## For attaching licensing metadata to pages, and displaying an
 ## appropriate copyright notice / icon. GNU Free Documentation
 ## License and Creative Commons licenses are supported so far.
